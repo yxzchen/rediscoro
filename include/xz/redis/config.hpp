3d5a6d5582e6b2b6b471aa9ce310fb8bef9a5e19
yxzchen <chenyxmr@gmail.com>
2025-12-12 15:45:41 +0800

    sync

---
+++ include/xz/redis/config.hpp
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <chrono>
+#include <optional>
+#include <string>
+
+namespace xz::redis {
+
+struct config {
+  std::string host = "localhost";
+  std::uint16_t port = 6379;
+  std::chrono::milliseconds connect_timeout{5000};
+  std::chrono::milliseconds request_timeout{5000};
+  std::optional<std::string> username;
+  std::optional<std::string> password;
+  int database = 0;
+  std::optional<std::string> client_name;
+  bool auto_reconnect = true;
+  std::optional<std::size_t> max_reconnect_attempts;
+  std::chrono::milliseconds reconnect_delay{100};
+  std::chrono::milliseconds max_reconnect_delay{30000};
+};
+
+}  // namespace xz::redis
